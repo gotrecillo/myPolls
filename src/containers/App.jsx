9f9bb7114c4fea1add30522ba892c9134c61fcef
gotrecillo <sergio.panadero.perez@gmail.com>
2015-11-29 13:09:23 +0100

    navbar style customization

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -7,21 +7,39 @@ export default class App extends Component {
 
   constructor(props) {
     super(props);
+    this.state = ({active: 2});
+  }
+
+  handleClick(num){
+    this.setState({active: num});
   }
 
   render() {
     return (
       <div>
       	<h1 className="text-center">Welcome to Poll App</h1>
-
-          <ul className="nav nav-tabs nav-justified text-center">
-            <li role="presentation"><Link to="/poll">Show Polls</Link></li>
-            <li role="presentation"><Link to="/">Hide Polls</Link></li>
-            <li role="presentation"><NotificationsContainer/></li>
+        <nav className="navbar navbar-default">
+          <ul className="nav navbar-nav">
+            <li 
+              className={`${this.state.active === 0 ? 'active-tab' : ''}`}
+              onClick={this.handleClick.bind(this, 0)}>
+              <Link to="/poll">Show Polls</Link>
+            </li>
+            <li 
+              className={`${this.state.active === 1 ? 'active-tab' : ''}`}
+              onClick={this.handleClick.bind(this, 1)}>
+              <Link to="/">Hide Polls</Link>
+            </li>
+            <li
+              className={`${this.state.active === 2 ? 'active-tab' : ''}`}
+              onClick={this.handleClick.bind(this, 2)}>
+              <NotificationsContainer/>
+            </li>
           </ul>
-          <div>
-            <ConfirmDialogContainer/>
-          </div>
+        </nav>
+      <div>
+        <ConfirmDialogContainer/>
+      </div>
   	    {this.props.children}
       </div>
     );
